ba79592ba439eb1cacd4ae64421abb27d3e09eec
Shridhar Kalagi <shridharkalagi@gmail.com>
2018-07-30 18:18:26 +0530

    removing comments

--- src/test/java/io/appium/java_client/android/AndroidFunctionTest.java
+++ src/test/java/io/appium/java_client/android/AndroidFunctionTest.java
@@ -93,9 +93,7 @@ public class AndroidFunctionTest extends BaseAndroidTest {
         assertThat("WebView is expected", driver.getContext(), containsString("WEBVIEW"));
     }
 
-    @Test
-    public void complexWaitingTestWithPostConditions() {
-        //Failing
+    @Test public void complexWaitingTestWithPostConditions() {
         final List<Boolean> calls = new ArrayList<>();
 
         AppiumFunction<Pattern, WebDriver> waitingForContext = input -> {
